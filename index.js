2e10ddac1b100f191065667a4a3fe85770f19e95
Arslan Ahmad <mycomputer2214@gmail.com>
2023-08-04 15:02:39 +0500

    Nearly, Completed:     - App is working Properly (Logics are also Working well)     - Facing an Error on showing winner

--- index.js
+++ index.js
@@ -36,22 +36,89 @@ const changePlayer = () => {
     }
 }
 
+// Checking if there is a winner
+
+const checkWinner = () => {
+
+    const boxes = document.querySelectorAll('.box')
+    const values = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    values.forEach((value) => {
+        const [a, b, c] = value
+        if (boxes[a].innerHTML === boxes[b].innerHTML && boxes[a].innerHTML === boxes[c].innerHTML && boxes[a].innerText !== '') {
+            boxes[a].classList.add('bg-teal-500/70')
+            boxes[b].classList.add('bg-teal-500/70')
+            boxes[c].classList.add('bg-teal-500/70')
+
+            winAnc(currentPlayerName)
+            boxes.forEach(box => box.disabled = true)
+            return
+        }
+    })
+}
+
 
 
 // Managing Box's Click
 const boxClicked = e => {
-    if (e.target.innerText === '') {
+
+    if (e.target.disabled !== true && e.target.innerText === '') {
+
         e.target.innerText = currentPlayer
         changePlayer()
         e.target.disabled = true
     }
 }
 
+// Game Ending Function 
+const end = () => {
+    if (id('winner').innerText !== '') {
+        id('end').classList.remove('hidden')
+        id('tieGame').classList.add('hidden')
+    } else {
+        id('end').classList.add('hidden')
+        id('tieGame').classList.remove('hidden')
+    }
+
+    document.querySelectorAll('.box').forEach(box => box.disabled = true)
+
+}
+
+
+// Reset Game
+const resetGame = () => {
+    currentPlayerName = player1
+    currentPlayer = 'X'
+
+    document.querySelectorAll('.box').forEach(box => {
+        if (box.disabled === true) {
+            box.disabled = false
+        }
+        if (box.classList.contains('bg-teal-600')) {
+            box.classList.remove('bg-teal-600')
+        }
+
+        box.innerHTML = ''
+    })
+}
+
 
 // Winner Announcement
 const name = 'User'
-const winAnc = name => {
-    id('winner').innerText = name;
+const winAnc = () => {
+
+    changePlayer()
+    id('winner').innerText = currentPlayerName;
+    id('winnerPlayer').innerText = currentPlayer;
     id('winAnnounce').classList.remove('hidden')
 }
 // winAnc(name)
